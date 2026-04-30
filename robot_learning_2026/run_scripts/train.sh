1249fcd611eb35c58f038850c9fc48265b449561
Sebastian <slochmann@ethz.ch>
2026-04-30 16:12:26 +0200

    update train script

--- robot_learning_2026/run_scripts/train.sh
+++ robot_learning_2026/run_scripts/train.sh
@@ -1,24 +1,14 @@
 # Train a diffusion policy on the collected dataset
 
-param(
-    [string]$Dataset = "grab-task",
-    [string]$Root = "robot_learning_2026/50_corner_grab",
-    [string]$OutputDir = "outputs/train/diffusion_grab",
-    [string]$JobName = "diffusion_grab",
-    [int]$BatchSize = 4,
-    [int]$Steps = 2000,
-    [string]$Policy = "diffusion"
-)
-
 lerobot-train \
-  --dataset.repo_id="local/$Dataset" \
-  --dataset.root=$Root \
-  --policy.type=$Policy \
-  --policy.repo_id="local/$JobName" \
-  --output_dir=$OutputDir \
-  --job_name=$JobName \
+  --dataset.repo_id="local/grab-task" \
+  --dataset.root="robot_learning_2026/50_corner_grab" \
+  --policy.type="diffusion" \
+  --policy.repo_id="diffusion_grab" \
+  --output_dir=outputs/train/diffusion_grab \
+  --job_name=diffusion_grab \
   --policy.device=cuda \
   --wandb.enable=true \
-  --batch_size=$BatchSize \
-  --steps=$Steps \
+  --batch_size=4 \
+  --steps=2000 \
   --policy.push_to_hub=true
