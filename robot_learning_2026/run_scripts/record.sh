2ac5a03b22181bf616bedf5e8d45ce068197adea
Sebastian <slochmann@ethz.ch>
2026-04-30 23:25:33 +0200

    record with hf upload and resume

--- robot_learning_2026/run_scripts/record.sh
+++ robot_learning_2026/run_scripts/record.sh
@@ -1,14 +1,15 @@
 
-DISPLAY=:0 lerobot-record \
+WINIT_UNIX_BACKEND=x11 lerobot-record \
   --robot.type=so101_follower --robot.port='/dev/ttyACM1' --robot.id=my_awesome_follower_arm \
   --teleop.type=so101_leader --teleop.port='/dev/ttyACM0' --teleop.id=my_awesome_leader_arm \
-  --robot.cameras="{ front: {type: opencv, index_or_path: 2, width: 480, height: 640, fps: 30, rotation: -90}}" \
+  --robot.cameras="{ front: {type: opencv, index_or_path: 0, width: 480, height: 640, fps: 30, rotation: -90}}" \
   --display_data=false \
-  --dataset.repo_id="slochmann/folding_task" \
-  --dataset.num_episodes=10 \
+  --dataset.repo_id="slochmann/50_fold" \
+  --dataset.num_episodes=40 \
   --dataset.streaming_encoding=true \
-  --dataset.encoder_threads=6 \
+  --dataset.encoder_threads=2 \
   --dataset.push_to_hub=true \
-  --dataset.root=outputs/fold_$(date +%Y%m%d_%H%M%S) \
-  --dataset.reset_time_s=0 \
+  --resume=true \
+  --dataset.root=outputs/50_fold\
+  --dataset.reset_time_s=10 \
   --dataset.single_task="1fold"
