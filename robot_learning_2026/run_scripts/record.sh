cebf535a2f6faef449f71bf727edc221e5fac64c
Tristan Gabl <trgabl@student.ethz.ch>
2026-05-11 22:30:07 +0200

    changes to record script

--- robot_learning_2026/run_scripts/record.sh
+++ robot_learning_2026/run_scripts/record.sh
@@ -5,13 +5,13 @@ if [[ "$(uname)" == "Darwin" ]]; then
     --teleop.type=so101_leader --teleop.port='/dev/tty.usbmodem5B140317801' --teleop.id=my_awesome_leader_arm \
     --robot.cameras="{ front: {type: opencv, index_or_path: 0, width: 480, height: 640, fps: 30, rotation: -90}}" \
     --display_data=false \
-    --dataset.repo_id="DerBoroter/100_first_fold_tristan" \
-    --dataset.num_episodes=18 \
+    --dataset.repo_id="DerBoroter/full_fold_tristan" \
+    --dataset.num_episodes=50 \
     --dataset.streaming_encoding=true \
     --dataset.encoder_threads=6 \
     --dataset.push_to_hub=true \
     --resume=true \
-    --dataset.root=outputs/100_first_fold_tristan \
+    --dataset.root=outputs/full_fold_tristan \
     --dataset.single_task="single_fold"
 
 else
