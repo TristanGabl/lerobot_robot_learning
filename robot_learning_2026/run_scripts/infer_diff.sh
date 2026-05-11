9743812ae2d4bf788dd12f6d5e3876393ad7aded
Sebastian <slochmann@ethz.ch>
2026-05-11 12:52:03 +0200

    resize image

--- robot_learning_2026/run_scripts/infer_diff.sh
+++ robot_learning_2026/run_scripts/infer_diff.sh
@@ -7,7 +7,7 @@ sudo chmod 666 /dev/ttyACM1
 lerobot-record \
   --robot.type=so101_follower --robot.port='/dev/ttyACM1' --robot.id=my_awesome_follower_arm \
   --robot.cameras="{ front: {type: opencv, index_or_path: 2, width: 480, height: 640, fps: 30, rotation: -90}}" \
-  --display_data=true \
+  --display_data=false \
   --dataset.single_task="fold" \
   --dataset.streaming_encoding=false \
   --dataset.encoder_threads=6 \
@@ -15,11 +15,12 @@ lerobot-record \
   --dataset.num_episodes=1 \
   --dataset.episode_time_s=90 \
   --policy.use_amp=true \
-  --policy.path="DerBoroter/diffusion_fold_2cm_crop" \
+  --policy.path="DerBoroter/diffusion_fold_2cm_ResNet18" \
   --policy.device=cuda \
   --policy.n_action_steps=15 \
   --interpolation_multiplier=1 \
   --dataset.fps=30 \
+  --policy.resize_shape=[240,320] \
   --policy.noise_scheduler_type="DDIM" \
   --dataset.push_to_hub=false \
   --policy.num_inference_steps=50
