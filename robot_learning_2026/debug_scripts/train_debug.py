12d52c048f530a2982cd09b5560a77d236945004
Tristan Gabl <trgabl@student.ethz.ch>
2026-05-15 10:44:17 +0200

    prepared shell script for training dit

--- robot_learning_2026/debug_scripts/train_debug.py
+++ robot_learning_2026/debug_scripts/train_debug.py
@@ -4,26 +4,30 @@ from pathlib import Path
 
 os.chdir(Path(__file__).parents[2])
 
-    
 sys.argv = [
     "debug_train.py",
-    "--dataset.repo_id=DerBoroter/full_fold_tristan",
+    "--dataset.repo_id=DerBoroter/50_corner_grab",
     "--policy.type=multi_task_dit",
-    "--policy.repo_id=DerBoroter/agnostic_dit",
-    "--output_dir=outputs/train/agnostic_dit",
-    "--job_name=dit_fold",
+    "--policy.horizon=32",
+    "--policy.n_action_steps=24",
+    "--policy.objective=diffusion",
+    "--policy.noise_scheduler_type=DDPM",
+    "--policy.num_train_timesteps=100",
+    "--policy.image_resize_shape=[320,240]",
+    "--policy.image_crop_shape=null",
+    "--policy.use_amp=true",
+    "--policy.repo_id=DerBoroter/DiT_grab",
+    "--output_dir=outputs/train/DiT_grab_debug",
+    "--job_name=DiT_grab",
     "--policy.device=cpu",
     "--batch_size=2",
     "--steps=3",
     "--save_freq=3",
     "--eval_freq=0",
-    "--policy.use_amp=true",
     "--num_workers=0",
     "--log_freq=1",
     "--save_checkpoint=false",
     "--policy.push_to_hub=false",
-    "--policy.image_resize_shape=[320,240]", 
-    "--policy.image_crop_shape=null"
 ]
 
 from lerobot.scripts.lerobot_train import main
--- robot_learning_2026/run_scripts/train_DiT.sh
+++ robot_learning_2026/run_scripts/train_DiT.sh
@@ -1,23 +1,23 @@
 # Train a diffusion policy on the collected dataset
 
 lerobot-train \
-  --dataset.repo_id="DerBoroter/50_corner_grab" \
+  --dataset.repo_id="DerBoroter/full_fold_tristan" \
+  --policy.repo_id="DerBoroter/full_fold_tristan_dit" \
   --policy.type="multi_task_dit" \
-  --policy.horizon=32 \
-  --policy.n_action_steps=24 \
-  --policy.horizon=32 \
+  --policy.horizon=64 \
+  --policy.n_action_steps=32 \
+  --policy.drop_n_last_frames=31 \
   --policy.objective=diffusion \
-  --policy.noise_scheduler_type=DDPM \
-  --policy.num_train_timesteps=100 \
+  --policy.image_resize_shape=[320,240] \
+  --policy.image_crop_shape=null \
   --policy.use_amp=true \
-  --batch_size=200 \
-  --steps=30000 \
-  --policy.repo_id="DerBoroter/DiT_grab" \
+  --batch_size=64 \
+  --steps=50000 \
+  --save_freq=10000 \
+  --eval_freq=5000 \
   --output_dir=outputs/train/DiT_grab_$(date +%Y%m%d_%H%M%S) \
   --job_name=DiT_grab \
   --policy.device=cuda \
   --wandb.enable=true \
-  --wandb.project=derRoboter \
-  --save_freq=10000 \
-  --eval_freq=5000 \
-  --policy.push_to_hub=true
+  --policy.push_to_hub=true \
+  --num_workers=8
