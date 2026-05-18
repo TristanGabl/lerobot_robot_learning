10cdf976b0451d7ebeb89e6ae08a18e58a939f39
Tristan Gabl <trgabl@student.ethz.ch>
2026-05-18 12:05:35 +0200

    updaded train_DiT.sh

--- robot_learning_2026/run_scripts/train_DiT.sh
+++ robot_learning_2026/run_scripts/train_DiT.sh
@@ -1,8 +1,9 @@
 # Train a diffusion policy on the collected dataset
 
 lerobot-train \
-  --dataset.repo_id="DerBoroter/full_fold_tristan" \
-  --policy.repo_id="DerBoroter/full_fold_tristan_dit" \
+  --dataset.repo_id="DerBoroter/full_fold_tristan_300eps" \
+  --policy.repo_id="DerBoroter/full_fold_tristan_300eps_dit" \
+  --output_dir=outputs/train/full_fold_tristan_300eps_$(date +%Y%m%d_%H%M%S) \
   --policy.type="multi_task_dit" \
   --policy.horizon=64 \
   --policy.n_action_steps=32 \
@@ -13,21 +14,20 @@ lerobot-train \
   --policy.timestep_embed_dim=384 \
   --policy.drop_n_last_frames=31 \
   --policy.objective=diffusion \
-  --policy.image_resize_shape=[448,336] \
-  --policy.image_crop_shape=[336,336] \
+  --policy.image_resize_shape=[341,256] \
+  --policy.image_crop_shape=[256,256] \
   --policy.vision_num_keypoints=64 \
   --policy.image_crop_shape=null \
   --policy.use_amp=true \
-  --batch_size=1 \
+  --batch_size=64 \
   --steps=50000 \
   --save_freq=10000 \
   --eval_freq=5000 \
-  --output_dir=outputs/train/DiT_grab_$(date +%Y%m%d_%H%M%S) \
   --job_name=DiT_grab \
   --policy.device=cuda \
   --wandb.enable=true \
   --policy.push_to_hub=true \
   --policy.private=true \
   --num_workers=8 \
-  '--dataset.image_transforms.tfs={"identity":{"type":"Identity","weight":0.7,"kwargs":{}},"color_jitter":{"type":"ColorJitter","weight":0.2,"kwargs":{"brightness":[0.5,1.5],"contrast":[0.8,1.2],"saturation":[0.8,1.2],"hue":[-0.03,0.03]}}}' \
+  '--dataset.image_transforms.tfs={"identity":{"type":"Identity","weight":0.8,"kwargs":{}},"color_jitter":{"type":"ColorJitter","weight":0.2,"kwargs":{"brightness":[0.5,1.5],"contrast":[0.8,1.2],"saturation":[0.8,1.2],"hue":[-0.03,0.03]}}}' \
   --dataset.transforms_refresh=100000
