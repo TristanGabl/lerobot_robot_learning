df69156c576c90d5f65c68d20b9a2d6994065fb8
Tristan Gabl <trgabl@student.ethz.ch>
2026-05-19 01:05:24 +0200

    new train_diff_new script, running faster brightness augmentation

--- robot_learning_2026/run_scripts/train_diff_new.sh
+++ robot_learning_2026/run_scripts/train_diff_new.sh
@@ -9,14 +9,14 @@ lerobot-train \
   --policy.device=cuda \
   --wandb.enable=true \
   --wandb.project=derRoboter \
-  --batch_size=1 \
-  --steps=50000 \
+  --batch_size=64 \
+  --steps=70000 \
   --save_freq=10000 \
   --eval_freq=10000000 \
   --policy.resize_shape=[320,192] \
   --policy.crop_shape=null \
   --policy.use_amp=true \
-  --num_workers=8 \
+  --num_workers=16 \
   --policy.push_to_hub=true \
   --policy.private=true \
   --policy.horizon=64 \
@@ -25,11 +25,13 @@ lerobot-train \
   --policy.pretrained_backbone_weights="dinov3_vits16" \
   --policy.backbone_lr_factor=0.1 \
   --policy.use_group_norm=false \
+  --dataset.video_backend=pyav \
   --dataset.image_transforms.enable=true \
   --dataset.image_transforms.max_num_transforms=1 \
   --dataset.image_transforms.random_order=false \
-  '--dataset.image_transforms.tfs={"identity":{"type":"Identity","weight":0.8,"kwargs":{}},"color_jitter":{"type":"ColorJitter","weight":0.2,"kwargs":{"brightness":[0.5,1.5],"contrast":[0.8,1.2],"saturation":[0.8,1.2],"hue":[-0.03,0.03]}}}'
+  '--dataset.image_transforms.tfs={"identity":{"type":"Identity","weight":0.7,"kwargs":{}},"color_jitter":{"type":"ColorJitter","weight":0.3,"kwargs":{"brightness":[0.7,1.3]}}}' \
   
+
 
 # ResNet18_Weights.IMAGENET1K_V1 for resnet18
 # dinov3_vits16 for dinov3 backbone 
