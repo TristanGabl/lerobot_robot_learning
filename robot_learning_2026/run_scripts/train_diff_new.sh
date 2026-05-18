98de834e7cf274580f4ad3c3fa468701e6aea5c5
Tristan Gabl <trgabl@student.ethz.ch>
2026-05-18 11:32:32 +0200

    update train_diff_new.sh

--- robot_learning_2026/run_scripts/train_diff_new.sh
+++ robot_learning_2026/run_scripts/train_diff_new.sh
@@ -1,9 +1,9 @@
 # Train a diffusion policy on the collected dataset
 
 lerobot-train \
-  --dataset.repo_id="DerBoroter/full_fold_general_tristan" \
+  --dataset.repo_id="DerBoroter/full_fold_tristan_300eps" \
   --policy.type="diffusion" \
-  --policy.repo_id="DerBoroter/full_fold_general_tristan_resnet18" \
+  --policy.repo_id="DerBoroter/full_fold_tristan_300eps_dino" \
   --output_dir=outputs/train/full_fold_tristan_with_recovery_$(date +%Y%m%d_%H%M%S) \
   --job_name=diffusion_fold \
   --policy.device=cuda \
@@ -13,7 +13,8 @@ lerobot-train \
   --steps=50000 \
   --save_freq=10000 \
   --eval_freq=10000000 \
-  --policy.resize_shape=[320,240] \
+  --policy.resize_shape=[341,256] \
+  --policy.crop_shape=[256,256] \
   --policy.use_amp=true \
   --num_workers=8 \
   --policy.push_to_hub=true \
@@ -21,14 +22,14 @@ lerobot-train \
   --policy.horizon=64 \
   --policy.n_action_steps=32 \
   --policy.drop_n_last_frames=31 \
-  --policy.pretrained_backbone_weights="ResNet18_Weights.IMAGENET1K_V1" \
+  --policy.pretrained_backbone_weights="dinov3_vits16" \
   --policy.backbone_lr_factor=0.1 \
   --policy.use_group_norm=false \
   --dataset.image_transforms.enable=true \
   --dataset.image_transforms.max_num_transforms=1 \
   --dataset.image_transforms.random_order=false \
-  '--dataset.image_transforms.tfs={"identity":{"type":"Identity","weight":0.7,"kwargs":{}},"color_jitter":{"type":"ColorJitter","weight":0.2,"kwargs":{"brightness":[0.5,1.5],"contrast":[0.8,1.2],"saturation":[0.8,1.2],"hue":[-0.03,0.03]}}}' \
-  --dataset.transforms_refresh=10000
+  '--dataset.image_transforms.tfs={"identity":{"type":"Identity","weight":0.8,"kwargs":{}},"color_jitter":{"type":"ColorJitter","weight":0.2,"kwargs":{"brightness":[0.5,1.5],"contrast":[0.8,1.2],"saturation":[0.8,1.2],"hue":[-0.03,0.03]}}}' \
+  --dataset.transforms_refresh=0
   
 
 # ResNet18_Weights.IMAGENET1K_V1 for resnet18
